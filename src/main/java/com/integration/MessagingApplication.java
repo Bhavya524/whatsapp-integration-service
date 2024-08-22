29d8a43cea4cb205ae262822a0ddbd749bb7e7b6
Bhavya Patel <bhavya.patel@raapyd.com>
2024-08-22 17:35:30 +0530

    rename class

--- src/main/java/com/integration/MessagingApplication.java
+++ src/main/java/com/integration/MessagingApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class IntegrationService {
+public class MessagingApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(IntegrationService.class, args);
+        SpringApplication.run(MessagingApplication.class, args);
         System.out.println("********* \uD83D\uDE00 Application is Up and Running \uD83D\uDE00 *********");
 
     }
