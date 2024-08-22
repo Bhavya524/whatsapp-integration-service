06bb0a4d5493c7d15baf7ddc44715588d9de6df9
Bhavya Patel <bhavya.patel@raapyd.com>
2024-08-22 17:33:36 +0530

    set up with spring initializer

---
+++ src/main/java/com/integration/IntegrationService.java
@@ -0,0 +1,15 @@
+package com.integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrationService {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntegrationService.class, args);
+        System.out.println("********* \uD83D\uDE00 Application is Up and Running \uD83D\uDE00 *********");
+
+    }
+
+}
